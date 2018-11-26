e1c5fe0e70a7d13a2259e4c5bb46dc3269fd1fb2
IDreDD <apfedcup@gmail.com>
2018-11-26 14:17:22 +0100

    .cup terminé

--- src/main/java/fr/usmb/m1isc/compilation/tp/NoeudType.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/NoeudType.java
@@ -3,17 +3,30 @@ package fr.usmb.m1isc.compilation.tp;
 public enum NoeudType {
 
     ENTIER,
+
+    // logique
+
     AND,
 	OR,
     EGAL,
-    GT,
-    GTE,
+    IF,
+    ACTION, // then ou else
+
+    //comparateurs
+
+    GT,       // "<"
+    GTE,      // "<="
+
+    //operateurs
+
     PLUS,
     MOINS,
     MOINS_UNAIRE,
     MUL,
     DIV,
     MOD,
+
+    // autre
     OUTPUT,
     INPUT,
     WHILE,
